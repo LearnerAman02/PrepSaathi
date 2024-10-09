d9f3a78453e6d1c82db0b455d33ad9eebfb99f54
Aman Singh <amansinghab23@gmail.com>
2024-10-10 04:59:39 +0530

    files uploaded

---
+++ server/database/data.js
@@ -0,0 +1,76 @@
+export default [
+  {
+    id: 1,
+    question: "Javascript is an _______ language",
+    options: ["Object-Oriented", "Object-Based", "Procedural"],
+  },
+  {
+    id: 2,
+    question:
+      "Following methods can be used to display data in some form using Javascript",
+    options: ["document.write()", "console.log()", "window.alert()"],
+  },
+  {
+    id: 3,
+    question:
+      "When an operator value is NULL, the typeof returned by the unary operator is:",
+    options: ["Boolean", "Undefined", "Object"],
+  },
+  {
+    id: 4,
+    question: "What does the toString() method return?",
+    options: ["Return Object", "Return String", "Return Integer"],
+  },
+  {
+    id: 5,
+    question:
+      "Which function is used to serialize an object into a JSON string?",
+    options: ["stringify()", "parse()", "convert()"],
+  },
+];
+
+export const answers = [0, 1, 2, 1, 0];
+
+// export default [
+//   questions: [
+//     {
+//       id: 1,
+//       question:
+//         "When there is a change in magnetic flux,______ is induced in _________ coil",
+//       options: [
+//         "EMF, Non-Conducting",
+//         "EMF,Conducting",
+//         "Current, Non-Conducting",
+//       ],
+//     },
+//     {
+//       id: 2,
+//       question:
+//         "If ball is released from height of 10m, it will hit the ground with velocity _____",
+//       options: ["10", "20", "15"],
+//     },
+//     {
+//       id: 3,
+//       question:
+//         "If the magnet is revolved above the disc in clockwise direction, in which direction disc will the disc move _____",
+//       options: ["clockwise", "anti-clockwise", "along the magnet"],
+//     },
+//     {
+//       id: 4,
+//       question: "Frictional Force acting on an object is ______",
+//       options: [
+//         "Conservative Force",
+//         "Non Conservative Force",
+//         "Path Independent Force",
+//       ],
+//     },
+//     {
+//       id: 5,
+//       question:
+//         "The time of flight for a body with velocity 30m/s projected at an angle of 60 degree with vertical is _____",
+//       options: ["4", "6", "3"],
+//     },
+//   ],
+// ];
+
+// export const answers = [1, 1, 0, 1, 2];
