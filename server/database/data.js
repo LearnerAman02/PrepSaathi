421106d6b3502319e39377dcabbc82714d8a8a4d
Aman Singh <128976778+LearnerAman02@users.noreply.github.com>
2024-10-18 21:10:02 +0530

    Added JEE Advanced questions

--- server/database/data.js
+++ server/database/data.js
@@ -1,76 +1,73 @@
 export default [
   {
     id: 1,
-    question: "Javascript is an _______ language",
-    options: ["Object-Oriented", "Object-Based", "Procedural"],
+    question:
+      "A heavy nucleus Q of half-life 20 minutes undergoes alpha-decay with a probability of 60% and beta-decay with a probability of 40%.Initially, the number of Q nuclei is 1000.The number of alpha-decays of Q in the first one hour is",
+    options: ["50", "75", "350", "525"],
+    steps: [
+      "Step 1: Understanding of concept of Modern Physics chapter.",
+      "Step 2: Radioactive decay concept understanding.",
+      "Step 3: Derive general equation for Number of nucleons.",
+      "Step 4: Use equation and determine Half life.",
+      "Step 5: Find the decay after given time period.",
+    ],
   },
   {
     id: 2,
     question:
-      "Following methods can be used to display data in some form using Javascript",
-    options: ["document.write()", "console.log()", "window.alert()"],
+      "A uniform electric field, E =-400√3 y NC is applied in a region.A charged particle of mass m carrying positive charge q is projected in this region with an initial speed of 2√10 x (10^6) m/s.This particle is aimed to hit a target T, which is 5m away from its entry point into the field, take q/m = 10^10 C/kg. Then,",
+    options: [
+      "Particle will hit T if projected at an angle 45 from horizontal",
+      "Particle will hit T if projected either at an angle 30 or 60 from horizontal",
+      "time taken by the particle to hit T could be √(5/7) micro sec as well as √(5/2) micro sec",
+      "time taken by the particle to hit T is √(5/3) micro sec",
+    ],
+    steps: [
+      "Step 1: Concept of Electrostatics and Projectile Motion.",
+      "Step 2: Determine acceleration in each direction.",
+      "Step 3: Calculate general displacement along x direction for given angles.",
+      "Step 4: Calculate general Time of Flight.",
+      "Step 5: Substitute values for angle of projection in time of flight equation.",
+    ],
   },
   {
     id: 3,
     question:
-      "When an operator value is NULL, the typeof returned by the unary operator is:",
-    options: ["Boolean", "Undefined", "Object"],
+      "A stone is projected at angle 30 degree to horizontal.The ratio of kinetic energy of the stone at point of projection to its kinetic energy at the highest point of flight will be:",
+    options: ["1:2", "1:4", "4:1", "4:3"],
+    steps: [
+      "Step 1: Understand Projectile motion concept.",
+      "Step 2: Perform component breaking of the velocities.",
+      "Step 3: Analyse net velocity at point of projection.",
+      "Step 4: Analyse net velocity at highest point.",
+      "Step 5: Calculate ratio of Kinetic Energy.",
+    ],
   },
   {
     id: 4,
-    question: "What does the toString() method return?",
-    options: ["Return Object", "Return String", "Return Integer"],
+    question:
+      "A conducting circular loop is placed in a uniform magnetic field of 0.4T with its plane perpendicular to the field.Somehow, the radius of the loop starts expanding at a constant rate of 1 mm/s. The magnitude of induced EMF in the loop at an instant when the radius of the loop is 2cm will be _____ micro Volt.",
+    options: ["60", "40", "50", "45"],
+    steps: [
+      "Step 1: Understanding of the Magnetic Flux.",
+      "Step 2: Identify the B, A vector direction.",
+      "Step 3: EMF induction reason in the given situation.",
+      "Step 4: Formula of EMF.",
+      "Step 5: Calculate the EMF value.",
+    ],
   },
   {
     id: 5,
-    question:
-      "Which function is used to serialize an object into a JSON string?",
+    question: "",
     options: ["stringify()", "parse()", "convert()"],
+    steps: [
+      "Step 1: Understand the role of JSON in data exchange.",
+      "Step 2: Identify the purpose of serialization.",
+      "Step 3: Learn that 'JSON.stringify()' serializes data.",
+      "Step 4: Compare with 'JSON.parse()' for deserialization.",
+      "Step 5: Validate that 'stringify()' is used for serialization.",
+    ],
   },
 ];
 
-export const answers = [0, 1, 2, 1, 0];
-
-// export default [
-//   questions: [
-//     {
-//       id: 1,
-//       question:
-//         "When there is a change in magnetic flux,______ is induced in _________ coil",
-//       options: [
-//         "EMF, Non-Conducting",
-//         "EMF,Conducting",
-//         "Current, Non-Conducting",
-//       ],
-//     },
-//     {
-//       id: 2,
-//       question:
-//         "If ball is released from height of 10m, it will hit the ground with velocity _____",
-//       options: ["10", "20", "15"],
-//     },
-//     {
-//       id: 3,
-//       question:
-//         "If the magnet is revolved above the disc in clockwise direction, in which direction disc will the disc move _____",
-//       options: ["clockwise", "anti-clockwise", "along the magnet"],
-//     },
-//     {
-//       id: 4,
-//       question: "Frictional Force acting on an object is ______",
-//       options: [
-//         "Conservative Force",
-//         "Non Conservative Force",
-//         "Path Independent Force",
-//       ],
-//     },
-//     {
-//       id: 5,
-//       question:
-//         "The time of flight for a body with velocity 30m/s projected at an angle of 60 degree with vertical is _____",
-//       options: ["4", "6", "3"],
-//     },
-//   ],
-// ];
-
-// export const answers = [1, 1, 0, 1, 2];
+export const answers = [3, 1, 3, 2, 0];
